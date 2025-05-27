f2e2617eb75a4e7dd67620625491297e947ab134
Pascal <bule@zhaw.ch>
2025-05-27 07:06:21 +0000

    fixed SI bug in datalaoder

--- utils/tram_data.py
+++ utils/tram_data.py
@@ -5,11 +5,8 @@ from torchvision import transforms
 from utils.tram_model_helpers import ordered_parents
 
 class GenericDataset(Dataset):
-    def __init__(self, df, target_col, conf_dict=None, transform=None):
-        
-        
+    def __init__(self, df, target_col, conf_dict=None, transform=None, transformation_terms_in_h=None):
         #TODO if intercept is si but shifts are ci , intercept should return 1s
-        
         """
         Args:
             df (pd.DataFrame): The dataframe containing data.
@@ -22,18 +19,15 @@ class GenericDataset(Dataset):
         self.conf_dict = conf_dict
         self.target_col = target_col
         self.transform = transform
-
+        self.transformation_terms_in_h=transformation_terms_in_h
     def __len__(self):
         return len(self.df)
 
     def __getitem__(self, idx):
-        
-        
-        
-        
         row = self.df.iloc[idx]
         x_data = []
-        # if source node
+        
+        # if source node only 1 for SI and the target (node itself) is returned
         if self.conf_dict is None:
             y = torch.tensor(row[self.target_col], dtype=torch.float32)
             x = torch.tensor(1.0) # For SI on Sources CI also possible but not meaningful
@@ -41,7 +35,12 @@ class GenericDataset(Dataset):
             x = tuple(x_data)
             return x , y
         
-        # data loader if not source
+        # if there are no Intercepts we need to add a 1 because modell assumes SI for intercepts
+        if all('i' not in str(value) for value in self.transformation_terms_in_h.values()):
+            x = torch.tensor(1.0) 
+            x_data.append(x)
+        
+        # data loader if not source , differnt format for the datatypes
         for var in self.variables:
             if self.conf_dict[var] == "cont":
                 x_data.append(torch.tensor(row[var], dtype=torch.float32))
@@ -55,7 +54,6 @@ class GenericDataset(Dataset):
                     image = self.transform(image)
                     
                 x_data.append(image)  # Append instead of replacing by index
-
         x = tuple(x_data)
         y = torch.tensor(row[self.target_col], dtype=torch.float32)
 
@@ -73,26 +71,21 @@ def get_dataloader(node, conf_dict, train_df, val_df, batch_size=32,verbose=Fals
             transforms.ToTensor()
         ])
     
-    
-    
     if conf_dict[node]['node_type'] == 'source':
         print('>>>>>>>>>>>>  source node --> x in dataloader contains just 1s ') if verbose else None
         
         train_dataset = GenericDataset(train_df, target_col=node, conf_dict=None, transform=transform)
         validation_dataset = GenericDataset(train_df, target_col=node, conf_dict=None, transform=transform)
-        
     
     else:
         # create a datatype dictionnary for the dataloader to read the datatype --->> TODO can be passed to a args 
         # parents_dict={x[0]:x[1] for x  in  zip(conf_dict[node]['parents'],conf_dict[node]['parents_datatype'])}
         
-        parents_dataype_dict,_,_=ordered_parents(node, conf_dict)
+        parents_dataype_dict,transformation_terms_in_h,_=ordered_parents(node, conf_dict)
         
         
-        train_dataset = GenericDataset(train_df, target_col=node, conf_dict=parents_dataype_dict, transform=transform)
-        validation_dataset = GenericDataset(val_df, target_col=node, conf_dict=parents_dataype_dict, transform=transform)
-     
-     
+        train_dataset = GenericDataset(train_df, target_col=node, conf_dict=parents_dataype_dict, transform=transform,transformation_terms_in_h=transformation_terms_in_h)
+        validation_dataset = GenericDataset(val_df, target_col=node, conf_dict=parents_dataype_dict, transform=transform,transformation_terms_in_h=transformation_terms_in_h)
     # TODO add args to the datloader via config file    
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4,pin_memory=True)
     val_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=False, num_workers=4,pin_memory=True)
